7d97197c98c2cb984b98cadf7559a73d49cd0f37
MAHESWARI-ROUT <maheswarirout2005@gmail.com>
2026-06-20 15:05:32 +0530

    update both for global and local deployment

--- PULSE/frontend/src/components/Analytics.jsx
+++ PULSE/frontend/src/components/Analytics.jsx
@@ -30,7 +30,7 @@ export default function Analytics({ apiBase }) {
     .then(r => setData(r.data))
     .catch(() => {})
 
-  axios.get('${import.meta.env.VITE_API_BASE}/feedback-stats')
+ axios.get(`${import.meta.env.VITE_API_BASE}/feedback-stats`)
     .then(res => setFeedbackStats(res.data))
     .catch(console.error)
 
